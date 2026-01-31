14a07f662189a8ef34ef6f53ce96534dfb101115
Chandler McCook <chandler.mccook@gmail.com>
2026-01-31 15:28:45 -0600

    test classes

---
+++ Doodle Cloud Run Server/Doodle Cloud Run Server/Endpoints/Players.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Server;
+
+[Route("api/")]
+public class Players : ControllerBase
+{
+    public Players()
+    {
+
+    }
+
+    [HttpPost]
+    [Route("/players/{clientId}")]
+    public async Task<ActionResult> CreateNewPlayer([FromBody] PlayerCreateRequest body, string clientId){
+        Player player = new Player();
+
+        player.clientId = clientId;
+        player.name = body.data;
+
+        return StatusCode(200);
+
+    }
+}
---
+++ Doodle Cloud Run Server/Doodle Cloud Run Server/Models/Player.cs
@@ -0,0 +1,23 @@
+﻿namespace Server;
+
+public class Player
+{
+    public Player()
+    {
+
+    }
+
+    public string name { get; set; }
+    public string clientId { get; set; 
+    
+    }
+}
+
+public class PlayerCreateRequest
+{
+    public PlayerCreateRequest() { }
+
+    public string data { get; set; }
+    public Player playerData { get; set; }
+    public List<Player> players { get; set; }
+}
